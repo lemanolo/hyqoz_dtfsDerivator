548cc4609db69c1f82b9f3c6d4f43ae2df75bb39
Manuel López-Enríquez <carlos.manuel.lopez@gmail.com>
2014-06-24 13:21:20 -0500

    no message

--- run_from_java.sh
+++ run_from_java.sh
@@ -1,3 +1,3 @@
 export CONFIG_FILE=$1
 cd /Users/aguacatin/Research/HADAS/PhD/Prolog/hyqoz_dtfsDerivator
-/opt/local/bin/gprolog --init-goal "['load.pl','${CONFIG_FILE}']" --entry-goal "derive_dtfs_from_java(TYPES,DTFS),nl,write('TYPES = '),write(TYPES),nl,write('DTFS = '),write(DTFS), write(';'),nl,nl" --query-goal "halt" | egrep "TYPES|DTFS"
+/opt/local/bin/gprolog --init-goal "['load.pl','${CONFIG_FILE}']" --entry-goal "derive_dtfs_from_java(TYPES,DTFS),nl,write('TYPES = '),write(TYPES),nl,write('DTFS = '),write(DTFS), nl,nl" --query-goal "halt" | egrep "TYPES|DTFS"
